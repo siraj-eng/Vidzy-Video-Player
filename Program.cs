3e7e79c034afc152d299760d9fdec5f87a8105f3
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-21 09:15:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy_Video_Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
